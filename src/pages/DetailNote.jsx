1b6dd3338808851eeebbff007da2240c95aa07f5
habibalmaula <habibalmaula@gmail.com>
2024-11-26 10:33:25 +0700

    fix detail note

--- src/pages/DetailNote.jsx
+++ src/pages/DetailNote.jsx
@@ -2,8 +2,9 @@ import { useParams } from "react-router-dom";
 import { getNote } from "../utils";
 import { useState } from "react";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.bubble.css';
-
+import "react-quill/dist/quill.bubble.css";
+import HeaderApp from "../components/HeaderApp";
+import NotFound from "./base/NotFound";
 
 function DetailNote() {
   const { id } = useParams();
@@ -12,9 +13,18 @@ function DetailNote() {
   console.log(`DetailNote: ${JSON.stringify(note)}`);
 
   return (
-    <div className="w-full overflow-auto">
-      <h1 className="text-2xl font-semibold mb-5">{note.title}</h1>
-      <ReactQuill value={note.body} readOnly={true} theme={"bubble"} />
+    <div className="w-full">
+      {note ? (
+        <>
+          <HeaderApp />
+          <div className="bg-white p-5 rounded-md">
+            <h1 className="text-2xl font-semibold mb-5">{note.title}</h1>
+            <ReactQuill value={note.body} readOnly={true} theme={"bubble"} />
+          </div>
+        </>
+      ) : (
+        <NotFound />
+      )}
     </div>
   );
 }
